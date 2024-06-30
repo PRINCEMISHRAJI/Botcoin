0c23f9f0e0c7c3832933a5211de4bb07f0ea9850
unknown <hellcyberworld@gmail.com>
2024-06-30 18:18:27 +0530

    Created A Telegram Mini App called Botcoin

---
+++ src/images/index.js
@@ -0,0 +1,17 @@
+import botcoin from './botcoin.png'
+import trophy from './trophy.png'
+import bear from './bear.png'
+import coin from './coin.png'
+import rocket from './rocket.png'
+import highVoltage from './high-voltage.png'
+
+
+
+export {
+    botcoin,
+    trophy,
+    bear,
+    coin,
+    rocket,
+    highVoltage
+}
